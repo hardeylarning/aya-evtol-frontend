7cdf0849aaf063fa4d7afbcf6a8dc2efc64464a4
hardeylarning <hardeylarning@gmail.com>
2023-02-12 14:20:26 +0100

    evtol constructor and properties were added

--- src/app/model/evtol.ts
+++ src/app/model/evtol.ts
@@ -1,2 +1,11 @@
 export class Evtol {
+    constructor(
+        public _id: string,
+        public serialNumber: string,
+        public model: string,
+        public weight: number,
+        public batteryCapacity: number,
+        public state: string,
+        public medicines: []
+    ){}
 }
