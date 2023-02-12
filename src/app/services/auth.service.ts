eb592eaf2cf696a69f7a828769e628f77db29a7b
hardeylarning <hardeylarning@gmail.com>
2023-02-12 14:40:37 +0100

    auth service implementation completed

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, tap } from 'rxjs';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private _isLoggedIn$ = new BehaviorSubject<boolean>(false)
+  isLoggedIn$ = this._isLoggedIn$.asObservable()
 
-  constructor() { }
+  constructor(private userService: UserService) { 
+    const token = localStorage.getItem('token')
+    this._isLoggedIn$.next(!!token)
+  }
+
+  login(username: string, password: string) {
+    return this.userService.login(username, password).pipe(
+      tap((res:any) => {
+        this._isLoggedIn$.next(true)
+        localStorage.setItem('userId', res.data.userId)
+        localStorage.setItem('userName', res.data.fullname)
+        localStorage.setItem('token', res.data.token)
+      })
+    )
+  }
+
+  logout() {
+    localStorage.removeItem('token')
+    localStorage.removeItem('userId')
+    localStorage.removeItem('userName')
+  }
 }
